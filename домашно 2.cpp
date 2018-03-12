fd4f7eaebeb8374d6ede76ab35e3590c6318a69d
TishiP <36618518+TishiP@users.noreply.github.com>
2018-03-12 08:32:21 +0200

    Add files via upload

---
+++ домашно 2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{ int n;
+cout << "n= " ;
+cin >> n;
+
+if (n<=0)
+{
+cout << "incorrect input! \n" ;
+}
+else {
+    cout<< "correct input! \n ";
+}
+return 0;
+}
+
+
+
+/** int main ()
+{ int a[100]
+cout<<”n= ”;
+cin<<n;
+int n;
+if (!cin)
+{cout<<”error. bad input! \n”;
+return 1;
+}
+if (n<=0)
+{
+cout<<”incorrect input! \n”;
+}
+return 0;
+}
+ */
+
+
